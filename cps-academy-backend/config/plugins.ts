af8c76fdb2932e1c46ff4a36e3823cce29c8755c
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-06-28 23:03:35 +0600

    Setup Email Provider.

--- cps-academy-backend/config/plugins.ts
+++ cps-academy-backend/config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+// path: ./config/plugins.js
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT', 587),
+        secure: env.bool('SMTP_SECURE', false),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM_EMAIL'),
+        defaultReplyTo: env('SMTP_REPLY_TO_EMAIL'),
+      },
+    },
+  },
+  // ... other plugins
+});
